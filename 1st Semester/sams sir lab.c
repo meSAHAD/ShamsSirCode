66864976ed92d8bdff007aa1f70b0ba5177cf9f5
Md. Sadekur Rahman Sahad <sahad.cse8.bu@gmail.com>
2024-10-10 00:47:18 +0600

    10octUpdate

--- 1st Semester/sams sir lab.c
+++ 1st Semester/sams sir lab.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 int main()
 {
-    int n, sum,num,fla;
+    int n, sum, num, fla;
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         scanf("%d", &num);
         int flag = 1;
-        for(int j = 2; j *j < num; j++){
-            if(num % j == 0) {
+        for (int j = 2; j * j < num; j++)
+        {
+            if (num % j == 0)
+            {
                 flag = 0;
                 break;
             }
         }
-        if(flag) sum =sum+num;
+        if (flag)
+            sum = sum + num;
     }
 
     printf("%d\n", sum);
-    for(int i = 2; i <sum/2; i++)
-    { int fla;
-        if (sum%i==0){fla=1;}
-
+    for (int i = 2; i < sum / 2; i++)
+    {
+        int fla;
+        if (sum % i == 0)
+        {
+            fla = 1;
+        }
     }
-    if (fla) {printf(" Prime");}
-        else {printf("Not Prime");}
+    if (fla)
+    {
+        printf(" Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    }
 }
